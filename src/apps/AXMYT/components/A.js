e0daa2a0b045e1955c0bdef09bbeab5cf3a8eeff
MadCoderme <63955762+MadCoderme@users.noreply.github.com>
2023-01-08 20:07:15 +0600

    Delete src/apps/AXMYT directory

--- src/apps/AXMYT/components/A.js
+++
@@ -1,5 +0,0 @@
-export default function A({label}) {
-    return (
-        <h2 style={{color: 'red'}}>{label}</h2>
-    )
-}
--- src/apps/AXMYT/src/test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function HelloWorld() {
-    return (
-        <h1>Hello world, guys</h1>
-    )
-}
