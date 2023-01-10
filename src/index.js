180ebff99b3153070ee84dd3ada66449e5b84ad1
MadCoderme <63955762+MadCoderme@users.noreply.github.com>
2023-01-10 21:43:54 +0600

    Update from Reax

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 export default function App() { 
-    const [txt, setTxt] = useState('')
+    const [txt, setTxt] = useState('') 
     
     return (
         <>
