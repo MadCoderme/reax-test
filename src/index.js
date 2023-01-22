188988a758359e6d952025afad9b4c2553b94763
MadCoderme <63955762+MadCoderme@users.noreply.github.com>
2023-01-22 20:03:15 +0600

    Update from Reax

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 export default function App() { 
-    const [txt, setTxt] = useState('') 
-     
     return (
-        <> 
-            <button onClick={() => {
-                console.log('hello') 
-            }}>
-                Click me!
-            </button> 
-        </>
+        <h1>Hello world</h1>
     )
 }
